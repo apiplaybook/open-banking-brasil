969bc9a1266c44e01be8395951abd332ba678a6a
Henry Kimura <htkimura@gmail.com>
2021-02-02 20:16:04 -0300

    refactor: change background from all app pages

--- src/styles/App.ts
+++ src/styles/App.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const AppStyled = styled.div`
 	display: flex;
 	flex-direction: column;
-	background: linear-gradient(180deg, #cc092f 0%, #b2207b 100%);
+	background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
 	min-height: 100vh;
 `
 
