66b3a4896f49affb9cc8dc38921bc6905f4d3ab2
Henry Kimura <htkimura@gmail.com>
2021-02-03 21:34:17 -0300

    refactor: change call open banking system

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,10 +4,7 @@ export const callOpenBanking = async (baseURL: string, endpoint: string) => {
 	const api = axios.create({
 		baseURL: `${baseURL}/open-banking/products-services/v1`,
 	})
+	const response = await api.get(endpoint)
 
-	return await api.get(endpoint)
-}
-export const callOpenBankingApiPersonalLoans = async (baseURL: string) => {
-	const apiResponse = await callOpenBanking(baseURL, '/personal-loans')
-	return apiResponse.data.data.brand
+	return response.data.data.brand
 }
