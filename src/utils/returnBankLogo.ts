84677d6abd70f7d6b7999017e520b4ca977b4978
Henry Kimura <htkimura@gmail.com>
2021-02-03 21:37:27 -0300

    feat: create return bank logo util function

---
+++ src/utils/returnBankLogo.ts
@@ -0,0 +1,24 @@
+import bbLogo from '../assets/img/bb-logo.jpg'
+import bancoPanLogo from '../assets/img/banco-pan-logo.svg'
+import bradescoLogo from '../assets/img/bradesco-logo.png'
+import itauLogo from '../assets/img/itau-logo.webp'
+import nextLogo from '../assets/img/next-logo.svg'
+
+export const returnBankLogo = (brandName: string) => {
+	switch (brandName) {
+		case 'Banco do Brasil S/A':
+			return bbLogo
+
+		case 'Grupo Pan':
+			return bancoPanLogo
+
+		case 'Banco Bradesco':
+			return bradescoLogo
+
+		case 'Banco Digital Next':
+			return nextLogo
+
+		case 'Itaú':
+			return itauLogo
+	}
+}
