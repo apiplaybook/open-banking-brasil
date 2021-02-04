08db76707be3fe1c7315799037b41d9591f5526b
Henry Kimura <htkimura@gmail.com>
2021-02-04 20:36:48 -0300

    feat: add companies field to bank const

--- src/constants/banks.ts
+++ src/constants/banks.ts
@@ -1,12 +1,14 @@
-export interface IBanksProps {
+export interface IBankProps {
 	brandName: string
 	apiUrl: string
 	style: any
 	logoWidth: string
+	companies: string[]
 }
-export const banks: IBanksProps[] = [
+export const banks: IBankProps[] = [
 	{
 		brandName: 'Banco do Brasil S/A',
+		companies: ['Banco do Brasil S/A'],
 		apiUrl: 'https://bb-api.concore.io',
 		style: {
 			backgroundColor: '#FBAB7E',
@@ -16,6 +18,7 @@ export const banks: IBanksProps[] = [
 	},
 	{
 		brandName: 'Grupo Pan',
+		companies: ['Banco Pan'],
 		apiUrl: 'https://bancopan-api.concore.io',
 		style: {
 			backgroundImage:
@@ -25,12 +28,14 @@ export const banks: IBanksProps[] = [
 	},
 	{
 		brandName: 'Banco Bradesco',
+		companies: ['Banco Bradesco S.A.'],
 		apiUrl: 'https://api.bradesco.com/bradesco',
 		style: { background: 'linear-gradient(180deg, #cc092f 0%, #b2207b 100%)' },
 		logoWidth: '50px',
 	},
 	{
 		brandName: 'Itaú',
+		companies: ['BANCO ITAU CONSIGNADO S.A.', 'BANCO ITAU UNIBANCO S.A.'],
 		apiUrl: 'https://itau-api.concore.io',
 		style: {
 			background: 'linear-gradient( 0deg,  #ff9d00 11.2%, rgba(255,0,0,1) 100.2% )',
@@ -39,6 +44,7 @@ export const banks: IBanksProps[] = [
 	},
 	{
 		brandName: 'Banco Digital Next',
+		companies: ['Banco Bradesco S.A.'],
 		apiUrl: 'https://api.bradesco.com/next',
 		style: {
 			background: 'linear-gradient( 110deg,  #1D3B3B 11.2%, #3cc974 100.2% )',
