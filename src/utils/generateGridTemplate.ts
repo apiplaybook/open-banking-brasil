93aadcd4763f5eeafb50066ac4c6b7b05e6836da
Henry Kimura <htkimura@gmail.com>
2021-02-12 13:42:51 -0300

    fix: max-width bug

--- src/utils/generateGridTemplate.ts
+++ src/utils/generateGridTemplate.ts
@@ -3,7 +3,7 @@ import { IBankProps } from './../constants/banks'
 export const generateGridTemplate = (banks: IBankProps[]) => {
 	let template = '200px'
 	banks.forEach((bank) => {
-		template += ` ${130 * bank.companies.length}px`
+		template += ` ${bank.companies.length === 0 ? 130 : 130 * bank.companies.length}px`
 	})
 
 	return template
