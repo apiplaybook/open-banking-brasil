de870df28c7249f70c007f27b7dfa93b210ee9f1
Henry Kimura <htkimura@gmail.com>
2021-02-12 13:09:17 -0300

    styles: clean up app routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,9 @@ import HomePage from './pages/Home'
 import BusinessLoansPage from './pages/BusinessLoans'
 import PersonalLoansPage from './pages/PersonalLoans'
 import PersonalAccountsPage from './pages/PersonalAccounts'
-import BusinessAccountsPage from './pages/BusinessAccounts'
-import {
-	HOME,
-	PERSONAL_LOANS,
-	BUSINESS_LOANS,
-	PERSONAL_ACCOUNTS,
-	PERSONAL_CREDIT_CARDS,
-	BUSINESS_ACCOUNT,
-} from './routes'
+
+import { HOME, PERSONAL_LOANS, BUSINESS_LOANS, PERSONAL_ACCOUNTS } from './routes'
 import AppStyled from './styles/App'
-import PersonalCreditCardsPage from './pages/PersonalCreditCards'
 
 function App() {
 	return (
@@ -24,9 +16,7 @@ function App() {
 				<Switch>
 					<Route exact component={BusinessLoansPage} path={BUSINESS_LOANS} />
 					<Route exact component={PersonalLoansPage} path={PERSONAL_LOANS} />
-					<Route exact component={BusinessAccountsPage} path={BUSINESS_ACCOUNT} />
 					<Route exact component={PersonalAccountsPage} path={PERSONAL_ACCOUNTS} />
-					<Route exact component={PersonalCreditCardsPage} path={PERSONAL_CREDIT_CARDS} />
 					<Route component={HomePage} path={HOME} />
 				</Switch>
 			</Router>
