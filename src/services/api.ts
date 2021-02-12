24cc3dd0c4d0118dff0f2dc39ad5683ec7e28eeb
Henry Kimura <htkimura@gmail.com>
2021-02-12 13:04:57 -0300

    feat: add availability check to call api function

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,32 @@
 import axios from 'axios'
+import { toCamelCase } from '../utils/toCamelCase'
+import { banks } from './../constants/banks'
 
 export const callOpenBanking = async (baseURL: string, endpoint: string) => {
 	const api = axios.create({
 		baseURL: `${baseURL}/open-banking/products-services/v1`,
 	})
-	const response = await api.get(endpoint)
 
-	return response.data.data.brand
+	const currentBank = banks.filter((bank) => bank.apiUrl === baseURL)[0]
+
+	const payloadName = toCamelCase(
+		endpoint.substr(1, endpoint.length - 1).replace(/[-\s]/g, ' ')
+	)
+
+	return await api
+		.get(endpoint)
+		.then((response) => response.data.data.brand)
+		.catch(() => {
+			return {
+				name: currentBank.brandName,
+				companies: [
+					{
+						name: 'INDISPONÍVEL',
+						cnpjNumber: '00000000000000',
+						urlComplementaryList: null,
+						[payloadName]: [],
+					},
+				],
+			}
+		})
 }
---
+++ src/utils/toCamelCase.ts
@@ -0,0 +1,6 @@
+export const toCamelCase = (str: string) =>
+	str
+		.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+			return index === 0 ? word.toLowerCase() : word.toUpperCase()
+		})
+		.replace(/\s+/g, '')
